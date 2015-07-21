80848ce7752a6ecc379b3de307cea56ae38483f5
Wesley R. Howell <wesleyrhowell@gmail.com>
2015-07-20 20:42:25 -0500

    Updated img.cpp to be more readable

--- graphics/img.cpp
+++ graphics/img.cpp
@@ -10,16 +10,26 @@
 #include <X11/Xos.h>
 #include <Imlib2.h>
 
+Display* dpy;
+Pixmap pix;
+Screen* scn;
+Window root;
+GC gc;
+
+Imlib_Image img;
+
+// Define some X functions
+bool InitX( int a_width, int a_height );
+bool InitImlib( const char* a_filename, int& a_width, int& a_height );
+void CloseX();
+void EventLoop();
+void Redraw();
+
 int main(int argc, char **argv)
 {
-    Imlib_Image img;
-    Display *dpy;
-    Pixmap pix;
-    Window root;
-    Screen *scn;
-    int width, height;
-    const char *filename = NULL;
-    GC gc;
+    int width;
+    int height;
+    const char* filename = NULL;
 
     if (argc < 2)
     {
@@ -28,84 +38,122 @@ int main(int argc, char **argv)
     }
     filename = argv[1];
 
-    img = imlib_load_image(filename);
-    if (!img) {
-        fprintf(stderr, "%s:Unable to load image\n", filename);
-        fprintf(stderr, "usage: %s <image_file>\n", argv[0]);
+    if( !InitImlib( filename, width, height ) )
+    {
+        fprintf( stderr, "Could not init imlib.\n" );
         return 1;
     }
-    imlib_context_set_image(img);
-    width = imlib_image_get_width();
-    height = imlib_image_get_height();
 
-    dpy = XOpenDisplay(NULL);
-    if (!dpy)
-        return 0;
-    scn = DefaultScreenOfDisplay(dpy);
-    root = DefaultRootWindow(dpy);
+    if( !InitX( width, height ) )
+    {
+        fprintf( stderr, "Could not init X.\n" );
+    }
 
-    pix = XCreatePixmap(dpy, root, width, height,
-        DefaultDepthOfScreen(scn));
+    EventLoop();
+
+    return 0;
+}
+
+bool InitX( int a_width, int a_height )
+{
+    dpy = XOpenDisplay( NULL );
+    if ( !dpy )
+    {
+        return false;
+    }
+
+    scn = DefaultScreenOfDisplay( dpy );
+    root = DefaultRootWindow( dpy );
+
+    pix = XCreatePixmap( dpy, root, a_width, a_height, DefaultDepthOfScreen(scn) );
 
     XSelectInput( dpy, root, ExposureMask|ButtonPressMask|KeyPressMask );
     gc = XCreateGC( dpy, root, 0, 0 );
-//    XSetBackground( dpy, gc, white );
-//    XSetForeground( dpy, gc, black );
     XClearWindow( dpy, root );
     XMapRaised( dpy, root );
 
-    imlib_context_set_display(dpy);
-    imlib_context_set_visual(DefaultVisualOfScreen(scn));
-    imlib_context_set_colormap(DefaultColormapOfScreen(scn));
-    imlib_context_set_drawable(pix);
+    imlib_context_set_display( dpy );
+    imlib_context_set_visual( DefaultVisualOfScreen( scn ) );
+    imlib_context_set_colormap( DefaultColormapOfScreen( scn ) );
+    imlib_context_set_drawable( pix );
 
-    imlib_render_image_on_drawable(0, 0);
-    XSetWindowBackgroundPixmap(dpy, root, pix);
-    XClearWindow(dpy, root);
+    imlib_render_image_on_drawable( 0, 0 );
+    XSetWindowBackgroundPixmap( dpy, root, pix );
+    XClearWindow( dpy, root );
 
-    while (XPending(dpy)) {
+    while( XPending( dpy ) )
+    {
         XEvent ev;
-        XNextEvent(dpy, &ev);
+        XNextEvent( dpy, &ev );
     }
+    return true;
+}
 
+bool InitImlib( const char* a_filename, int& a_width, int& a_height )
+{
+    img = imlib_load_image( a_filename );
+    if ( !img ) {
+        fprintf(stderr, "%s:Unable to load image\n", a_filename);
+        return false;
+    }
+    imlib_context_set_image( img );
+    a_width = imlib_image_get_width();
+    a_height = imlib_image_get_height();
+    return true;
+}
+
+void EventLoop()
+{
     XEvent event;
     char text[255];
     KeySym key;
     while( 1 )
     {
-                /* get the next event and stuff it into our event variable.
-                   Note:  only events we set the mask for are detected!
-                */
-                XNextEvent( dpy, &event );
+        // get the next event and stuff it into our event variable.
+        // Note:  only events we set the mask for are detected!
+        XNextEvent( dpy, &event );
 
-                if (event.type==Expose && event.xexpose.count==0)
-                {
-                        /* the window was exposed redraw it! */
-                        XClearWindow( dpy, root );
-                }
-                if (event.type==KeyPress&&
-                    XLookupString(&event.xkey,text,255,&key,0)==1)
-                {
-                /* use the XLookupString routine to convert the invent
-                   KeyPress data into regular text.  Weird but necessary...
-                */
-                        if (text[0]=='q') {
-                                XFreeGC( dpy, gc );
-                                XDestroyWindow( dpy, root );
-                                XCloseDisplay( dpy );
-                                exit( 1 );
-                        }
-                        printf("You pressed the %c key!\n",text[0]);
-                }
-                if (event.type==ButtonPress) {
-                /* tell where the mouse Button was Pressed */
-                        int x=event.xbutton.x;
-                        int y=event.xbutton.y;
-
-                        strcpy(text,"X is FUN!");
-                        XSetForeground( dpy, gc, rand()%event.xbutton.x%255 );
-                        XDrawString( dpy, root, gc, x, y, text, strlen(text) );
-                }
+        if( event.type==Expose && event.xexpose.count==0 )
+        {
+            // The window was exposed redraw it!
+            Redraw();
         }
-    return 0;
+        if( event.type==KeyPress &&
+            XLookupString(&event.xkey,text,255,&key,0)==1 )
+        {
+            // Use the XLookupString routine to convert the invent
+            // KeyPress data into regular text.  Weird but necessary...
+            if (text[0]=='q')
+            {
+                CloseX();
+            }
+            else
+            {
+                printf("You pressed the %c key!\n",text[0]);
+            }
+        }
+        if (event.type==ButtonPress)
+        {
+            // Tell where the mouse Button was Pressed
+            int x=event.xbutton.x;
+            int y=event.xbutton.y;
+
+            strcpy(text,"X is FUN!");
+            XSetForeground( dpy, gc, rand()%event.xbutton.x%255 );
+            XDrawString( dpy, root, gc, x, y, text, strlen(text) );
+        }
+    }
+}
+
+void CloseX()
+{
+    XFreeGC( dpy, gc );
+    XDestroyWindow( dpy, root );
+    XCloseDisplay( dpy );
+    exit( 1 );
+}
+
+void Redraw()
+{
+    XClearWindow( dpy, root );
 }
